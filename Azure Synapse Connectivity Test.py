5745a1e35e58d1fa7c6596f7ff43b96a95c5dca7
hb4pu <hb4pu>
2023-01-18 01:44:59 +0000

    changes

--- Azure Synapse Connectivity Test.py
+++ Azure Synapse Connectivity Test.py
@@ -8,7 +8,7 @@ dwDatabase = "dedsqlpoolbikeshare"
 dwServer = "synapse-udacitybikeshare2.sql.azuresynapse.net"
 dwUser = "sqladminuser"
 dwPass = "SqlAU$3r"
-#TODO: How to properly security password?
+#TODO: How to properly security password? check this article: https://learn.microsoft.com/en-us/azure/databricks/scenarios/databricks-extract-load-sql-data-warehouse
 dwJdbcPort =  "1433"
 dwJdbcExtraOptions = "encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
 sqlDwUrl = f"jdbc:sqlserver://{dwServer}:{dwJdbcPort};database={dwDatabase};user={dwUser};password={dwPass};${dwJdbcExtraOptions}"
@@ -18,10 +18,10 @@ sqlDwUrl = f"jdbc:sqlserver://{dwServer}:{dwJdbcPort};database={dwDatabase};user
 # tempDir = "wasbs://<<container>>@<<your-storage-account-name>>.blob.core.windows.net/<<folder-for-temporary-data>>"
 
 # Azure Data Lake Gen 2
-tempDir = "abfss://<<container >>@<<your-storage-account-name>>.dfs.core.windows.net/<<folder-for-temporary-data>>"
+tempDir = "abfss://bikeshareblobc@dlg2account.dfs.core.windows.net/tempspace"
 
 # Azure Synapse Table
-tableNameDimRider = "Dim.Rider"
+tableNameDimRider = "Numbers"
 
 df = spark.read \
   .format("com.databricks.spark.sqldw") \
@@ -32,3 +32,28 @@ df = spark.read \
   .load()
 
 display(df)
+
+# COMMAND ----------
+
+#Azure Synapse related settings
+dwDatabase = "dedsqlpoolbikeshare"
+dwServer = "synapse-udacitybikeshare2.sql.azuresynapse.net"
+dwUser = "sqladminuser"
+dwPass = "SqlAU$3r"
+dwJdbcPort =  "1433"
+dwJdbcExtraOptions = "encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+sqlDwUrl = "jdbc:sqlserver://" + dwServer + ":" + dwJdbcPort + ";database=" + dwDatabase + ";user=" + dwUser+";password=" + dwPass + ";$dwJdbcExtraOptions"
+sqlDwUrlSmall = "jdbc:sqlserver://" + dwServer + ":" + dwJdbcPort + ";database=" + dwDatabase + ";user=" + dwUser+";password=" + dwPass
+
+spark.conf.set(
+    "spark.sql.parquet.writeLegacyFormat",
+    "true")
+
+dfbronzeriders = spark.read.format("delta") \
+    .load("/delta/bronze/bronze_riders")
+display(dfbronzeriders)
+dfbronzeriders.write.format("com.databricks.spark.sqldw").option("url", sqlDwUrlSmall).option("dbtable", "SampleTable").option( "forward_spark_azure_storage_credentials","True").option("tempdir", tempDir).mode("overwrite").save()
+
+# COMMAND ----------
+
+
